47a631d6fa229d0722f0801ccc55c0fa84db0004
Habibur Rahman <habib.swi16@gmail.com>
2023-10-26 23:31:45 +0600

    initiated mongodb server

---
+++ index.js
@@ -0,0 +1,52 @@
+import express from "express";
+import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import "dotenv/config";
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// root
+//
+
+const user = process.env.DB_user;
+const password = process.env.DB_pass;
+
+const uri = `mongodb+srv://${user}:${password}@cluster0.1zxryai.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("server running");
+});
+
+app.listen(port, () => {
+  console.log("server running at ", port);
+});
