3411f3ff4bc531076d3d9461a453c727806b7e08
Habibur Rahman <habib.swi16@gmail.com>
2023-10-27 11:24:04 +0600

    get services data

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-// root
-//
+app.use(cors());
+app.use(express.json());
 
 const user = process.env.DB_user;
 const password = process.env.DB_pass;
@@ -27,7 +27,18 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const serviceCollection = client.db("car-genius").collection("services");
+
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+
+      const result = await cursor.toArray();
+
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -35,13 +46,10 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-
-app.use(cors());
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("server running");
