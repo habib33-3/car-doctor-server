bcd3892ed053b37bc502d7a0157ab96f6d9c1a86
Habibur Rahman <habib.swi16@gmail.com>
2023-10-28 13:28:54 +0600

    delete api

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function run() {
     // await client.connect();
 
     const serviceCollection = client.db("car-genius").collection("services");
+    const bookingCollection = client.db("car-genius").collection("booking");
 
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
@@ -44,12 +45,39 @@ async function run() {
       const query = { _id: new ObjectId(id) };
 
       const options = {
-        projection: { title: 1, price: 1, service_id: 1 },
+        projection: {
+          title: 1,
+          price: 1,
+          service_id: 1,
+          img: 1,
+        },
       };
 
       const result = await serviceCollection.findOne(query, options);
-     
+      res.send(result);
+    });
 
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.get("/bookings", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await bookingCollection.find(query).toArray();
+
+      res.send(result);
+    });
+
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
       res.send(result);
     });
 
